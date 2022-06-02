6534ab2ca6ab1a22af87ceda24a3637cfeb83b54
adrianpiveta <danielpiveta257@gmail.com>
2022-06-01 21:33:10 -0300

    Authent required

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import TodoSerializer
 from todo.models import Todo
 
 
 # Create your views here.
 class TodoList(generics.ListAPIView):
-    serializer_class=TodoSerializer
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
